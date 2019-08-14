5f33a150cb270477da28ac5025484a07ba75c7b0
Samuel Torres <rilla@Samuels-iMac.local>
2019-08-14 15:09:58 -0400

    Samuel-Torres

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import Card from './Card';
+import axios from 'axios';
 
 function App() {
+
+  const [api, setapi] = useState();
+
+  console.log('Api Results:', api);
+
+  useEffect(() => {
+    axios
+      .get('https://api.nasa.gov/api.html#apod')
+      .then(res => {
+        console.log(res);
+        const results = res.data.results
+        setapi(results);
+      })
+      .catch(err => {
+        console.log('Sorry Try Again Later', err);
+      })
+  }, [])
+
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <Card />
     </div>
   );
 }
